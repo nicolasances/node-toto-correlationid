4a74cf07d1762f1fed2ea7f34c417fff5bd6a4e1
nicolas <nicolas.matteazzi@gmail.com>
2019-02-09 09:50:14 +0100

    Generator of Toto Correlation ids

---
+++ README.md
@@ -0,0 +1,10 @@
+# Toto Correlation Id
+This is a generator for correlation identifiers used in toto
+
+## Usage
+```
+var cid = require('toto-correlationid');
+
+let correlationId = cid();
+```
+That's all!!
---
+++ index.js
@@ -0,0 +1,14 @@
+var moment = require('moment-timezone');
+
+/**
+ * This module generates a correlation Id
+ */
+module.exports.cid = function() {
+
+  let ts = moment().tz('Europe/Rome').format('YYYYMMDDHHmmssSSS');
+
+  let random = (Math.random() * 100000).toFixed(0).padStart(5, '0');
+
+  return tz + '-' + random;
+
+}
